c1e58053ef4e1321e5ed193df42fc37be09a5555
Wouter Lievens <lievenswouter@gmail.com>
2016-03-10 23:26:03 +0100

    cleanup

--- src/main/java/heraldry/render/Painter.java
+++ src/main/java/heraldry/render/Painter.java
@@ -4,23 +4,23 @@ import heraldry.model.Tincture;
 
 public interface Painter
 {
-    double getOrdinaryThickness();
-    
-    Paint getColor(Tincture tincture);
-    
     Color getOrdinaryBorderColor();
-    
+
     Color getOuterBorderColor();
-    
-    double getGridPatternSize();
-    
-    double getFrettyPatternSize();
-    
-    double getFretMargin();
-    
-    double getFretSizeStep();
-    
-    double getLinePeriodFactor();
-    
+
     double getChiefHeight();
+
+    double getFretMargin();
+
+    double getFretSizeStep();
+
+    double getFrettyPatternSize();
+
+    double getGridPatternSize();
+
+    double getLinePeriodFactor();
+
+    double getOrdinaryThickness();
+
+    Paint getColor(Tincture tincture);
 }
