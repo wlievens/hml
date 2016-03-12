228e4e68548ca3b8fcc118b74e64a6d2cf210da8
Wouter Lievens <lievenswouter@gmail.com>
2016-03-12 15:38:30 +0100

    utility function for keeping the xsd uptodate

--- src/main/java/heraldry/util/EnumUtils.java
+++ src/main/java/heraldry/util/EnumUtils.java
@@ -7,4 +7,12 @@ public class EnumUtils
         String name = value.name();
         return Character.toUpperCase(name.toLowerCase().charAt(0)) + name.toLowerCase().substring(1).replace("_", " ");
     }
+
+    public static <T extends Enum<T>> void printAsXsd(T[] values)
+    {
+        for (T value : values)
+        {
+            System.out.println("<xs:enumeration value=\"" + value.name().toLowerCase().replace("_", "-") + "\"/>");
+        }
+    }
 }
