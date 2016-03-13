cadec6bd3e0ddcc7b288393583cbb594dcf8a0b9
Wouter Lievens <lievenswouter@gmail.com>
2016-03-13 17:41:21 +0100

    started work on pall ordinary

---
+++ src/main/java/heraldry/render/ordinary/PallOrdinaryRenderer.java
@@ -0,0 +1,42 @@
+package heraldry.render.ordinary;
+
+import heraldry.model.Line;
+import heraldry.render.Box;
+import heraldry.render.LinePathStep;
+import heraldry.render.LineRenderer;
+import heraldry.render.Painter;
+import heraldry.render.PathStep;
+import heraldry.render.RenderContour;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class PallOrdinaryRenderer implements OrdinaryRenderer
+{
+    private final double sizeRatio;
+    private final double flipY;
+
+    @Override
+    public Collection<RenderContour> render(Box bounds, Line line, Painter painter)
+    {
+        double x1 = bounds.getX1();
+        double y1 = bounds.getY1();
+        double x2 = bounds.getX2();
+        double y2 = bounds.getY2();
+        double width = bounds.getWidth();
+        double height = bounds.getHeight();
+        double step = sizeRatio * painter.getOrdinaryThickness() * line.getScaleFactor() / 2;
+        double midX = (x1 + x2) / 2;
+        double period = painter.getLinePeriodFactor() * Math.min(width, height);
+        List<PathStep> steps = new ArrayList<>();
+        steps.add(new LinePathStep(midX - step, y1, midX + step, y1));
+        LineRenderer.line(steps, midX + step, y1, midX + step, y2, line, period, false, sizeRatio);
+        steps.add(new LinePathStep(midX + step, y2, midX - step, y2));
+        LineRenderer.line(steps, midX - step, y2, midX - step, y1, line, period, false, sizeRatio);
+        return Arrays.asList(new RenderContour(steps));
+    }
+}
