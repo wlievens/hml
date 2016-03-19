f40436c4276fd9dd03a4e3a702ec9d6269b58bf5
Wouter Lievens <lievenswouter@gmail.com>
2016-03-19 23:19:32 +0100

    party per pale understands line now

--- src/main/java/heraldry/render/division/PaleDivisionRenderer.java
+++ src/main/java/heraldry/render/division/PaleDivisionRenderer.java
@@ -2,15 +2,17 @@ package heraldry.render.division;
 
 import heraldry.model.Line;
 import heraldry.render.Box;
+import heraldry.render.LinePathStep;
+import heraldry.render.LineRenderer;
 import heraldry.render.Painter;
+import heraldry.render.PathStep;
 import heraldry.render.Point;
 import heraldry.render.RenderContour;
 import heraldry.util.GeometryUtils;
 
-import java.awt.geom.Area;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PaleDivisionRenderer implements DivisionRenderer
 {
@@ -19,26 +21,22 @@ public class PaleDivisionRenderer implements DivisionRenderer
     {
         Box bounds = contour.getBounds();
         Point center = bounds.getFessPoint();
+        double x1 = bounds.getX1();
+        double x2 = bounds.getX2();
+        double y1 = bounds.getY1();
+        double y2 = bounds.getY2();
+        double cx = center.getX();
+        double period = painter.getLinePeriodFactor() * Math.min(bounds.getWidth(), bounds.getHeight());
 
-        Box[] boxes = new Box[]{
-                new Box(bounds.getX1(), bounds.getY1(), center.getX(), bounds.getY2()),
-                new Box(center.getX(), bounds.getY1(), bounds.getX2(), bounds.getY2())
-        };
+        List<PathStep> steps = new ArrayList<>();
+        steps.add(new LinePathStep(x1, y1, cx, y1));
+        LineRenderer.line(steps, cx, y1, cx, y2, line, period, false, 1.0);
+        steps.add(new LinePathStep(cx, y2, x1, y2));
+        steps.add(new LinePathStep(x1, y2, x1, y1));
 
-        return Arrays.stream(boxes)
-                .map(box -> {
-                    Area area = GeometryUtils.convertContourToArea(contour);
-                    area.intersect(GeometryUtils.convertBoxToArea(box));
-                    return area;
-                })
-                .map(GeometryUtils::convertAreaToContours)
-                .map(list -> {
-                    if (list.size() != 1)
-                    {
-                        throw new IllegalStateException();
-                    }
-                    return list.get(0);
-                })
-                .collect(Collectors.toList());
+        RenderContour left = contour.clip(new RenderContour(steps)).get(0);
+        RenderContour right = GeometryUtils.subtract(contour, left).get(0);
+
+        return Arrays.asList(left, right);
     }
 }
