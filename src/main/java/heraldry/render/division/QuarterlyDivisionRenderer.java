e4f0b070b0f0ad41d1c44e40bc227a29f8a6177b
Wouter Lievens <lievenswouter@gmail.com>
2016-03-30 22:36:26 +0200

    process shaped lines for quarterly (doesn't seem to work)

--- src/main/java/heraldry/render/division/QuarterlyDivisionRenderer.java
+++ src/main/java/heraldry/render/division/QuarterlyDivisionRenderer.java
@@ -2,16 +2,20 @@ package heraldry.render.division;
 
 import heraldry.model.Line;
 import heraldry.render.Box;
+import heraldry.render.LineRenderer;
 import heraldry.render.Painter;
 import heraldry.render.Point;
 import heraldry.render.RenderContour;
+import heraldry.render.path.LinePathStep;
+import heraldry.render.path.Path;
+import heraldry.render.path.PathStep;
 import heraldry.util.CollectionUtils;
 import heraldry.util.GeometryUtils;
 
 import java.awt.geom.Area;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class QuarterlyDivisionRenderer implements DivisionRenderer
 {
@@ -20,29 +24,44 @@ public class QuarterlyDivisionRenderer implements DivisionRenderer
     {
         Box bounds = contour.getBounds();
         Point center = bounds.getFessPoint();
+        double cx = center.getX();
+        double cy = center.getY();
+        double x1 = bounds.getX1();
+        double x2 = bounds.getX2();
+        double y1 = bounds.getY1();
+        double y2 = bounds.getY2();
+        double period = painter.getLinePeriodFactor() * Math.min(bounds.getWidth(), bounds.getHeight());
 
-        Box[] boxes = new Box[]{
-                new Box(bounds.getX1(), bounds.getY1(), center.getX(), center.getY()),
-                new Box(center.getX(), bounds.getY1(), bounds.getX2(), center.getY()),
-                new Box(center.getX(), center.getY(), bounds.getX2(), bounds.getY2()),
-                new Box(bounds.getX1(), center.getY(), center.getX(), bounds.getY2())
-        };
+        Area remainder = GeometryUtils.convertContourToArea(contour);
+        List<PathStep> steps;
 
-        return Arrays.stream(boxes)
-                .map(box -> {
-                    Area area = GeometryUtils.convertContourToArea(contour);
-                    area.intersect(GeometryUtils.convertBoxToArea(box));
-                    return area;
-                })
-                .map(GeometryUtils::convertAreaToContours)
-                .map(list -> {
-                    if (list.size() != 1)
-                    {
-                        throw new IllegalStateException();
-                    }
-                    return CollectionUtils.single(list);
-                })
-                .collect(Collectors.toList());
+        steps = new ArrayList<>();
+        steps.add(new LinePathStep(x1, cy, x1, y1));
+        steps.add(new LinePathStep(x1, y1, cx, y1));
+        LineRenderer.line(steps, cx, y1, cx, cy, line, period, false, 1.0);
+        LineRenderer.line(steps, cx, cy, x1, cy, line, period, false, 1.0);
+        RenderContour topLeft = CollectionUtils.single(contour.clip(new RenderContour(new Path(steps))));
+        remainder.subtract(GeometryUtils.convertContourToArea(topLeft));
+
+        steps = new ArrayList<>();
+        LineRenderer.line(steps, x2, y2, cx, cy, line, period, false, 1.0);
+        LineRenderer.line(steps, cx, cy, cx, y1, line, period, false, 1.0);
+        steps.add(new LinePathStep(cx, y1, x2, y1));
+        steps.add(new LinePathStep(x2, y1, x2, cy));
+        RenderContour topRight = CollectionUtils.single(contour.clip(new RenderContour(new Path(steps))));
+        remainder.subtract(GeometryUtils.convertContourToArea(topRight));
+
+        steps = new ArrayList<>();
+        LineRenderer.line(steps, cx, y2, cx, cy, line, period, false, 1.0);
+        LineRenderer.line(steps, cx, cy, x2, cy, line, period, false, 1.0);
+        steps.add(new LinePathStep(x2, cy, x2, y2));
+        steps.add(new LinePathStep(x2, y2, cx, y2));
+        RenderContour bottomRight = CollectionUtils.single(contour.clip(new RenderContour(new Path(steps))));
+        remainder.subtract(GeometryUtils.convertContourToArea(bottomRight));
+
+        RenderContour bottomLeft = CollectionUtils.single(GeometryUtils.convertAreaToContours(remainder));
+
+        return Arrays.asList(topLeft, topRight, bottomRight, bottomLeft);
     }
 
 }
